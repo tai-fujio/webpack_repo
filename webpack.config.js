2eec875bd5f1bc959a4dc358fb74f75c58e74275
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-05-04 00:14:57 +0900

    index.pugに変更

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,20 @@ module.exports= {
           },
         ],
       },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            }
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -43,7 +57,7 @@ module.exports= {
       filename: './stylesheets/main.css',
     }),
     new HtmlWebpackPlugin({
-      template: './src/templates/index.html',
+      template: './src/templates/index.pug',
     }),
     new CleanWebpackPlugin(),
   ],
