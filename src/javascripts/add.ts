c88543a6a537d4dbcec878485ba766077c750e72
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-05-05 02:30:56 +0900

    typescript install

---
+++ src/javascripts/add.ts
@@ -0,0 +1,3 @@
+export default (a: number, b: number): string => {
+  return `The result is ${a * b}.`;
+}
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,8 +3,10 @@ import '../stylesheets/main.scss';
 import Vue from 'vue';
 import Vue1 from './Vue1.vue';
 import App2 from './react1.jsx';
+import add from './add.ts';
 sample();
 App2();
+console.log(add(134,3415));
 new Vue({
   el: '#root2',
   render: (h) => h(Vue1),
