a842a86fb6659a043e8e251e08b3bd3188547280
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-05-05 18:44:36 +0900

    cssコンポーネント化

--- src/javascripts/Message.tsx
+++ src/javascripts/Message.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import styled from 'styled-components'
+
+const CssContainer = styled.div`
+background-color: green;
+color: white;
+padding: 1em;
+`;
 const Message: React.FC<{message: string}> = ({message}) => {
   // const { message } = props;
   return (
-    <div style={{backgroundColor: 'green', color: 'white'}}>
+    <CssContainer>
       {message}
-    </div>
+    </CssContainer>
   );
 }
 export default Message;
--- src/javascripts/react1.jsx
+++ src/javascripts/react1.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Message from './Message.tsx';
+
 const App2 = () => {
 const App = (props) => {
   return(
