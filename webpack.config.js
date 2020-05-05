e7c00719b454d2819b2884a6128999b7e7f68ca9
tai-fujio <51994563+tai-fujio@users.noreply.github.com>
2020-05-05 22:27:37 +0900

    ソースファイル名hash化

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,13 @@ module.exports= {
   // mode: 'production',
   mode: 'development',
   devtool: 'source-map',
-  entry : './src/javascripts/main.js',
+  entry : {
+    main: './src/javascripts/main.js',
+  },
   output: {
     path: path.resolve(__dirname,'./dist'),
     // filename: 'main2.js',
-    filename: './javascripts/main.js',
+    filename: './javascripts/[name]-[hash].js',
   },
   module: {
     rules: [
@@ -76,7 +78,7 @@ module.exports= {
             loader: 'file-loader',
             options: {
               esModule: false,
-              name: 'images/[name].[ext]',
+              name: 'images/[name]-[hash].[ext]',
               publicPath: '/',
             },
           },
@@ -110,7 +112,7 @@ module.exports= {
   plugins: [
     new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
-      filename: './stylesheets/main.css',
+      filename: './stylesheets/[name]-[hash].css',
     }),
     new HtmlWebpackPlugin({
       template: './src/templates/index.pug',
